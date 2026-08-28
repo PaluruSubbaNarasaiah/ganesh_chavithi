f4d772a3070bf256dd6af95fd0de88189829570c
PaluruSubbaNarasaiah <palurusubbanarasiah@gmail.com>
2026-08-28 23:37:36 +0530

    Revert "Use root assets on Vercel"
    This reverts commit 4f16533f1bc4f0b3f0baecffcd4a27f76ba15714.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: process.env.GITHUB_ACTIONS === 'true' ? '/ganesh_chavithi/' : '/',
+    base: '/ganesh_chavithi/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
